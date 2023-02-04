41953ad4471ed3091e1ded71131ccdc09c3ea233
Susan <dhitalsusan2@gmail.com>
2023-02-04 12:20:55 +0545

    Added exception handling feature in Nato Alphbet project

--- Nato_Alphabet_Project/main.py
+++ Nato_Alphabet_Project/main.py
@@ -6,8 +6,19 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 # Creating a new dictionary using data -->Dataframe
 # using the dictionary comprehension
 code_letter = {rows.letter: rows.code for (index, rows) in data.iterrows()}
+
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter you name : ").upper()
-# Using the list comprehension
-name_list = [code_letter[letter] for letter in word]
-print(name_list)
+def phonetic_word():
+    word = input("Enter you name : ").upper()
+    # Using the list comprehension
+    try:
+        name_list = [code_letter[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letter in the alphabet please")
+        phonetic_word()
+    else:
+        print(name_list)
+
+
+phonetic_word()
